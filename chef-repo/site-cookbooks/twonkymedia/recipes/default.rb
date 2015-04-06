29f665454d77433fb3c7854eafd8761e89b17e18
kunii <kuniiskywalker@gmail.com>
2015-04-07 01:44:34 +0900

    [modify] twonkyレシピ更新

--- chef-repo/site-cookbooks/twonkymedia/recipes/default.rb
+++ chef-repo/site-cookbooks/twonkymedia/recipes/default.rb
@@ -20,17 +20,10 @@ cookbook_file "/usr/local/src/libgcc-4.8.2-16.2.el7_0.i686.rpm" do
   owner "root"
   group "root"
 end
+
 package "libgcc-4.8.2-16.2.el7_0.i686" do
   action   :install
   source   "/usr/local/src/libgcc-4.8.2-16.2.el7_0.i686.rpm"
-  provider Chef::Provider::Package::Rpm
-end
-
-# twonkymedia依存ライブラリ
-%w{ld-linux.so.2 libstdc++.so.5}.each do |suffix|
-  package suffix do
-    action :install
-  end
 end
 
 ## twonkymediaのインストール
@@ -45,14 +38,17 @@ bash 'make and install twonkymedia' do
   cwd '/usr/local'
   code <<-EOH
 unzip -d /usr/local/twonky /usr/local/src/twonkymedia-i386-glibc-2.2.5-6.0.zip
-cp twonky/twonkymedia.sh /etc/init.d/twonkymedia
-chmod 755 /etc/init.d/twonkymedia
-systemctl load /etc/init.d/twonkymedia
+chmod 755 twonky/twonkymedia.sh
   EOH
   not_if { File.exists?("/usr/local/twonky") }
 end
 
-service "twonkymedia" do
-  supports :status => true, :restart => true, :reload => true
-  action [:start, :enable]
+# twonkymedia依存ライブラリ
+package ["ld-linux.so.2", "libstdc++.so.5"] do
+  flush_cache [:before]
 end
+
+# service "twonkymedia" do
+#   supports :status => true, :restart => true, :reload => true
+#   action [:start, :enable]
+# end
